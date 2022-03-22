7d1a3e05896b7fd1bee096f7cbbe65c97c884eb6
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2022-03-22 20:52:10 +0400

    Specified all the arguments of the function

--- main.py
+++ main.py
@@ -1,61 +1,54 @@
 import os
 
 import requests
-from dotenv import load_dotenv
+from dotenv import load_dotenv,dotenv_values
 from urllib.parse import urlparse
 
 
-def shorten_link(bitly_token, url):
-    headers = {
-        "Authorization": f"Bearer {bitly_token}"
-    }
+BITLY_TOKEN = dotenv_values(".env")["BITLY_TOKEN"]
 
+HEADERS = {
+"Authorization": f"Bearer {BITLY_TOKEN}"
+}
+
+def shorten_link(url):
     long_url = {"long_url":url}
 
-    response = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=headers,json=long_url)
+    response = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=HEADERS,json=long_url)
 
     response.raise_for_status()
 
     return response.json()["id"]
 
-def count_number_clicks(token, bitlink):
-    headers = {
-        "Authorization": f"Bearer {bitly_token}"
-    }
-
-    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary", headers=headers)
+def count_number_clicks(bitlink):
+    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary", headers=HEADERS)
 
     response.raise_for_status()
 
     return response.json()["total_clicks"]
 
 
-def is_bitlink(bitly_token, bitlink):
-    headers = {
-        "Authorization": f"Bearer {bitly_token}"
-    }
-
-    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}", headers=headers)
+def is_bitlink(bitlink):
+    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}", headers=HEADERS)
 
     return response.ok
 
+
 if __name__ == "__main__":
     load_dotenv()
-
-    bitly_token = os.getenv('BITLY_TOKEN')
 
     url = input()
 
     parts_link = urlparse(url)
     glued_link = f"{parts_link.netloc}{parts_link.path}"
 
-    if is_bitlink(bitly_token, glued_link) == True:
+    if is_bitlink(glued_link) == True:
         try:
-            print(count_number_clicks(bitly_token, glued_link))
+            print(count_number_clicks(glued_link))
         except requests.exceptions.HTTPError:
             print("Ошибка")
     else:
         try:
-            print(shorten_link(bitly_token, url))
+            print(shorten_link(url))
         except requests.exceptions.HTTPError:
             print("Ошибка, неверная ссылка")
