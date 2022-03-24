adf1afe04e677870e28c8b13344419f6b123be57
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2022-03-24 20:44:12 +0400

    Formatted the code

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import os
 
 import requests
-from dotenv import load_dotenv,dotenv_values
+from dotenv import load_dotenv, dotenv_values
 from urllib.parse import urlparse
 
 
 BITLY_TOKEN = dotenv_values(".env")["BITLY_TOKEN"]
 
 HEADERS = {
-"Authorization": f"Bearer {BITLY_TOKEN}"
+    "Authorization": f"Bearer {BITLY_TOKEN}"
 }
 
-def shorten_link(url):
-    long_url = {"long_url":url}
 
-    response = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=HEADERS,json=long_url)
+def shorten_link(url):
+    long_url = {"long_url": url}
+
+    response = requests.post("https://api-ssl.bitly.com/v4/shorten",
+                             headers=HEADERS,
+                             json=long_url)
 
     response.raise_for_status()
 
     return response.json()["id"]
 
+
 def count_number_clicks(bitlink):
-    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary", headers=HEADERS)
+    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/"
+                            "{bitlink}/clicks/summary",
+                            headers=HEADERS)
 
     response.raise_for_status()
 
@@ -29,7 +35,8 @@ def count_number_clicks(bitlink):
 
 
 def is_bitlink(bitlink):
-    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}", headers=HEADERS)
+    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}",
+                            headers=HEADERS)
 
     return response.ok
 
@@ -42,7 +49,7 @@ if __name__ == "__main__":
     parts_link = urlparse(url)
     glued_link = f"{parts_link.netloc}{parts_link.path}"
 
-    if is_bitlink(glued_link) == True:
+    if is_bitlink(glued_link):
         try:
             print(count_number_clicks(glued_link))
         except requests.exceptions.HTTPError:
